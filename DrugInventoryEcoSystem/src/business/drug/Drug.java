33fbffa2eb9132f9fa4cea846f275bacfd8a41b7
RajSankhe <rajsankhe03@gmail.com>
2018-11-28 17:24:21 -0500

    Drug Class

---
+++ DrugInventoryEcoSystem/src/business/drug/Drug.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.drug;
+
+import java.util.List;
+
+/**
+ *
+ * @author Raj
+ */
+public class Drug {
+    private int id;
+    private String name;
+    private int quantity;
+    private double price;
+    private List<String> disease;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public List<String> getDisease() {
+        return disease;
+    }
+
+    public void setDisease(List<String> disease) {
+        this.disease = disease;
+    }
+    
+    
+}
