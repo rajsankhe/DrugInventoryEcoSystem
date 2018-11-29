0b8fdd048dd67235ae6c6d2dd5afd70bf696e216
RajNSankhe <sankhe.r@husky.neu.edu>
2018-11-29 10:32:26 -0600

    Changes

--- DrugInventoryEcoSystem/src/userinterface/chemist/workerrole/OrderDrugsJpanel.java
+++ DrugInventoryEcoSystem/src/userinterface/chemist/workerrole/OrderDrugsJpanel.java
@@ -63,16 +63,9 @@ public class OrderDrugsJpanel extends javax.swing.JPanel {
             Class[] types = new Class [] {
                 java.lang.String.class, java.lang.String.class
             };
-            boolean[] canEdit = new boolean [] {
-                true, false
-            };
 
             public Class getColumnClass(int columnIndex) {
                 return types [columnIndex];
-            }
-
-            public boolean isCellEditable(int rowIndex, int columnIndex) {
-                return canEdit [columnIndex];
             }
         });
         jScrollPane1.setViewportView(drugquantity);
