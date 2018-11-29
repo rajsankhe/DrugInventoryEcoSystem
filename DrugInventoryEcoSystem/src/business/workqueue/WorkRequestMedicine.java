c746adbc8de941abbe13910ba57959f08eb6955e
Tej Sankhe <sankhe.t@husky.neu.edu>
2018-11-28 19:02:54 -0500

    workqueue and workrequest

---
+++ DrugInventoryEcoSystem/src/business/workqueue/WorkRequestMedicine.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.workqueue;
+
+import business.drug.Drug;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Tej Sankhe
+ */
+public class WorkRequestMedicine extends WorkRequest{
+    List<Drug> drugsOrderList;
+
+    public WorkRequestMedicine() {
+    this.drugsOrderList = new ArrayList<>();
+    }
+
+    public List<Drug> getDrugsOrderList() {
+        return drugsOrderList;
+    }
+
+    public void setDrugsOrderList(List<Drug> drugsOrderList) {
+        this.drugsOrderList = drugsOrderList;
+    }
+    
+}
